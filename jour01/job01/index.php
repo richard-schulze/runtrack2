b5890ad00fc83d1e7085e1ce6b448f209758c226
richard-schulze <richard.schulze@laplateforme.io>
2023-01-09 11:30:23 +0100

    commit job01/index.php

---
+++ jour01/job01/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $str = "LaPlateforme";
+        echo $str;?> <br> <?php
+
+        $str2 = "Vive";
+        $str3 = "!";
+        echo $str2.$str.$str3;?><br>
+<?php
+    $val =6;
+    echo $val;?><br>
+    <?php
+    
+    echo ($val+4);?><br>
+    <?php
+    $myBool =true;
+    echo $myBool;
+    $myBool = false;
+    echo $myBool;
+    ?>
+</body>
+</html>
