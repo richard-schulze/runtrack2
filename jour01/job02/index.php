f2c8a3c5fd9ec31f97abfecb824e03d6eb7fb10b
richard-schulze <richard.schulze@laplateforme.io>
2023-01-09 11:47:52 +0100

    commit job2

---
+++ jour01/job02/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+       /* creation de ma variable
+          avec un commentaire sur 
+          plusieurs lignes */
+    $ma_variable = "Hello Laplateforme";
+    
+    // un commentaire sur une ligne affichage de 2fois ma_variable 
+    
+    echo $ma_variable.$ma_variable;
+
+    ?>
+</body>
+</html>
