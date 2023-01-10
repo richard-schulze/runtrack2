164bc8df3905ee46e76f60641f5e1a0872fd85bc
richard-schulze <richard.schulze@laplateforme.io>
2023-01-10 12:51:28 +0100

    commit job6/index.php

---
+++ jour02/job6/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    $hauteur = 10;
+    $largeur = 20;
+
+    if($largeur!=$hauteur){?>
+        <div style="background-color: black; height:<?php echo $hauteur ?>; width:<?php echo $largeur ?>"></div>
+<?php
+    } 
+?>
