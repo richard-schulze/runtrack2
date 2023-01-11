6916c10171861ecb014f3ba7801702ee6a0f0391
richard-schulze <richard.schulze@laplateforme.io>
2023-01-11 12:02:11 +0100

    commit job5/index.php

---
+++ jour03/job5/index.php
@@ -0,0 +1,32 @@
+<?php
+$str = "On n'est pas le meilleur quand on le croit mais quand on le sait";
+$dic = [
+    "consonnes" => "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvXxZz",
+    "voyelles" => "AaEeIiOoUuYy"
+];
+$voyelle = 0;
+$consonne = 0;
+
+for ($a = 0; isset($str[$a]); $a++) {
+    for ($i = 0; isset($dic["voyelles"][$i]); $i++) {
+        if ($str[$a] == $dic["voyelles"][$i]) {
+            $voyelle++;
+        }
+    }
+    for ($i = 0; isset($dic["consonnes"][$i]); $i++) {
+        if ($str[$a] == $dic["consonnes"][$i]) {
+            $consonne++;
+        }
+    }
+}
+echo " <table border=1>
+        <tr>
+            <th>consonnes</th>
+            <th>voyelles</th>
+        </tr>
+        <tr>
+            <td>$consonne</td>
+            <td>$voyelle</td>
+        </tr>
+        </table>";
+?>
