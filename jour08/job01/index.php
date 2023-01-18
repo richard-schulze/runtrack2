71c66fb2a31df735f0a83966bbd818e60647b922
richard-schulze <richard.schulze@laplateforme.io>
2023-01-18 14:10:19 +0100

    modif nouveau code

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -1,26 +1,37 @@
-<?php session_start() ?>
+<?php
+session_start();
 
-<!DOCTYPE html>
+if (!isset($_SESSION["nbvisites"])) {
+    $_SESSION["nbvisites"] = 0; 
+  }
+  else{
+    $_SESSION["nbvisites"] = $_SESSION["nbvisites"]+1;
+}
+
+echo $_SESSION["nbvisites"];
+
+if (isset($_GET["reset"]))
+ {
+    session_destroy();
+    header("location: index.php");
+}
+
+
+
+?>
+ 
+
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-<form action="" method="POST" class="formulaire">
-      <input name="button" type="submit" value="Reset">
-</form>
+    <form action="" method="get">
+        <input type="submit" name="reset" value="reset">
 
-<?php
-
-if(isset($_SESSION['nbvisites']) && !isset($_POST['button']))
-{
-      $_SESSION['nbvisites']++;
-      echo $_SESSION['nbvisites'];
-}
-else if (!isset($_SESSION['nbvisites']) || isset($_POST['button']))
-{
-      $_SESSION['nbvisites'] = 1;
-      echo $_SESSION['nbvisites'];
-}
- ?>
+    </form>
 </body>
 </html>
