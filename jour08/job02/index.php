1259f1d353555d9877b02a1b08401ec62bc2152c
richard-schulze <richard.schulze@laplateforme.io>
2023-01-18 14:41:26 +0100

    commit nouveau code

--- jour08/job02/index.php
+++ jour08/job02/index.php
@@ -1,19 +1,36 @@
 <?php
 
-if (!isset($_COOKIE['nbvisites']) || isset($_POST['button']))
-{
-      setcookie('nbvisites', '1', time()+3600);
-      header('Location: .');
-      echo $_COOKIE['nbvisites'];
+// if (!isset($_COOKIE['nbvisites']) || isset($_POST['button']))
+// {
+//       setcookie('nbvisites', '1', time()+3600);
+//       header('Location: .');
+//       echo $_COOKIE['nbvisites'];
 
 
+// }
+// else if (isset($_COOKIE['nbvisites']) && !isset($_POST['button']))
+// {
+//       $visits = (int)$_COOKIE['nbvisites'];
+//       $visits++;
+//       setcookie('nbvisites', (string)$visits, time()+3600);
+//       echo $_COOKIE['nbvisites'];
+// }
+if (!isset($_COOKIE["nbvisites"])){
+      $_COOKIE['nbvisites'] = 1;
+      setcookie("nbvisites", 1, time() + 36000);
+      echo $_COOKIE["nbvisites"];
+}else {
+      $_COOKIE["nbvisites"]++;
+      setcookie("nbvisites", $_COOKIE["nbvisites"], time() + 36000);
+      echo $_COOKIE["nbvisites"];
 }
-else if (isset($_COOKIE['nbvisites']) && !isset($_POST['button']))
+
+
+if (isset($_POST["reset"]))
 {
-      $visits = (int)$_COOKIE['nbvisites'];
-      $visits++;
-      setcookie('nbvisites', (string)$visits, time()+3600);
-      echo $_COOKIE['nbvisites'];
+      setcookie("nbvisites", "", time() - 36000);
+      unset($_POST["reset"]);
+      header("location:index.php");  
 }
 ?>
 
@@ -23,7 +40,7 @@ else if (isset($_COOKIE['nbvisites']) && !isset($_POST['button']))
 </head>
 <body>
 <form action="" method="POST" class="formulaire">
-      <input name="button" type="submit" value="Reset">
+      <input name="reset" type="submit" value="Reset">
 </form>
 
 
