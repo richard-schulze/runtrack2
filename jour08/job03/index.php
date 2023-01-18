7888fbd0d33671c1f35e9191be10202a94b72068
richard-schulze <richard.schulze@laplateforme.io>
2023-01-18 12:49:37 +0100

    commit job03/index.php

---
+++ jour08/job03/index.php
@@ -0,0 +1,35 @@
+<?php session_start();
+
+if (isset($_POST['reset']))
+{
+      session_destroy();
+      header('Location: .');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<form action="" method="POST" class="formulaire">
+      <input name="prenom" type="text">
+      <input name="send" type="submit" value="Envoyer">
+      <input name="reset" type="submit" value="Reset">
+</form>
+<?php
+if (isset($_POST['prenom']))
+{
+      $_SESSION['prenom'] =  $_SESSION['prenom'] . "<br><li>" . $_POST['prenom'] . "</li>";
+      echo "<ul>";
+      echo $_SESSION['prenom'];
+      echo "</ul>";
+}else{
+      echo "entrez votre prenom";
+}
+?>
+</body>
+</html>
+
