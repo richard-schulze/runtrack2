38295496d9a384dfa28fea5f69740e648ebceb5b
richard-schulze <richard.schulze@laplateforme.io>
2023-01-20 10:55:40 +0100

    commit job 01/index.php

---
+++ jour10/job 01/index.php
@@ -0,0 +1,37 @@
+ 
+    <!DOCTYPE html>
+    <html>
+    <head>
+    </head>
+    <body>
+    
+    <?php
+    $mysqli = new mysqli("localhost", "root", "", "jour09");
+    
+    $request = $mysqli->query("SELECT prenom,nom,naissance,sexe,email FROM etudiants;");
+    
+    $results = $request -> fetch_array(MYSQLI_ASSOC);
+    
+    
+    echo "<table border='1'><tr>";
+    
+    
+    foreach ($results as $key => $value)
+    {
+       echo " <th> " . $key . " </th> ";
+    }
+    echo "</tr>";
+    while ($results != NULL)
+    {
+       echo "<tr>";
+       foreach ($results as $value)
+       {
+             echo "<td>" . $value . "</td>";
+       }
+       echo "</tr>";
+       $results = $request -> fetch_array(MYSQLI_ASSOC);
+    }
+    echo "</table>";
+    ?>
+    </body>
+    </html>
